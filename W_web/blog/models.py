31559db8df5800839dcb61e5c7c6ba79d5679e6e
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-26 00:28:38 +0900

    게시물 사진추가

--- W_web/blog/models.py
+++ W_web/blog/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=15, null=False, verbose_name="게시물 이름")
     content = models.TextField(null=False ,verbose_name="게시물 내용")
+
+    head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
+
     created = models.DateTimeField(null=False, verbose_name="작성 시간")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
