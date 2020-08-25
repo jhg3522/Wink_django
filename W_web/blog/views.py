b3aaead0c9917ddcd4488db25f1c7f4598602529
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-25 15:20:44 +0900

    List view

--- W_web/blog/views.py
+++ W_web/blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import Post
-def index(request):
-    posts = Post.objects.all()
+from django.views.generic import ListView
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts
-        }
-    )
+class PostList(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created') #작성일의 역순으로 게시물 출력
+
+# def index(request):
+#     posts = Post.objects.all()
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts
+#         }
+#     )
