6fbf39faf56172ae65347c17389108a8727358c4
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-25 12:52:04 +0900

    post : str

--- W_web/blog/models.py
+++ W_web/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=15, null=False, verbose_name="게시물 이름")
+    content = models.TextField(null=False ,verbose_name="게시물 내용")
+    created = models.DateTimeField(null=False, verbose_name="작성 시간")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} :: {}'.format(self.title,self.author)
