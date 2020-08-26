c08a5af57a95f4f134c68bffcf5721ae9ff2e649
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-26 14:48:21 +0900

    base html

--- W_web/blog/tests.py
+++ W_web/blog/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from bs4 import BeautifulSoup
 
-# Create your tests here.
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text,'Blog')
--- W_web/blog/views.py
+++ W_web/blog/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
 
     def get_queryset(self):
         return Post.objects.order_by('-created') #작성일의 역순으로 게시물 출력력
+
+class PostDetail(DetailView):
+    model = Post
+
+# def post_detail(request, pk):
+#     blog_post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/post_detail_html',
+#         {
+#             'blog_post':blog_post,
+#         }
+#     )
 
 #def index(request):
 #     posts = Post.objects.all()
