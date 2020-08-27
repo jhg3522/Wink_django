69f1b122afd713ffdc3c76be31a0af41d00a22d1
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-27 20:00:17 +0900

    #3 구글로그인 로그아웃

--- W_web/W_web/urls.py
+++ W_web/W_web/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     path('blog/', include('blog.urls')),
     path('markdownx/', include('markdownx.urls')),
     path('accounts/', include('allauth.urls')),
