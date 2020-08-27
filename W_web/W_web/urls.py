2e5c039d63aa4f6fc4778c79fe7cef7a26a1a03c
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-27 19:26:07 +0900

    구글 로그인

--- W_web/W_web/urls.py
+++ W_web/W_web/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
