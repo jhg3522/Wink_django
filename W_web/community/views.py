699f80323194d016225dea505c654010db56fc51
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-31 19:20:58 +0900

    #2 community 실시간채팅 -2

--- W_web/community/views.py
+++ W_web/community/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from django.utils.safestring import mark_safe
+import json
 def index(request):
     return render(request, 'community/index.html',{})
+
+def room(request, room_name):
+    return render(request, 'community/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name))
+    })
