df2f6a8d974909c3357c2467342bf0b4e6a680ce
jhg3522 <jhg3522@kookmin.ac.kr>
2020-08-31 22:56:37 +0900

    #2 community 실시간채팅-3

--- W_web/W_web/routing.py
+++ W_web/W_web/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import community.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            community.routing.websocket_urlpatterns
+        )
+    ),
 })
--- W_web/community/views.py
+++ W_web/community/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
+
 def index(request):
     return render(request, 'community/index.html',{})
 
 def room(request, room_name):
     return render(request, 'community/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
     })
+
