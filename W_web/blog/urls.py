aca96aad68d0caceb9ffead8209b909baa7f487a
jhg3522 <jhg3522@kookmin.ac.kr>
2020-09-01 21:28:41 +0900

    댓글 수정 기능

--- W_web/blog/urls.py
+++ W_web/blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('category/<str:slug>/',views.PostListByCategory.as_view()),
     path('<int:pk>/update/',views.PostUpdate.as_view()),
     path('<int:pk>/new_comment/', views.new_comment),
+    path('edit_comment/<int:pk>/', views.CommentUpdate.as_view()),
     path('delete_comment/<int:pk>/', views.delete_comment),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('create/',views.PostCreate.as_view()),
